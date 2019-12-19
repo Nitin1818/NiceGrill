31a974fae6ee915e6acecc8fc85e9a66f790e28a
erenmetesar <erenmetesar@gmail.com>
2019-12-19 20:14:58 +0300

    song cmd added

--- nicegrill/modules/youtube.py
+++ nicegrill/modules/youtube.py
@@ -24,8 +24,8 @@ class YouTube:
             text += f"<i>◍ {i['title']}</i>\nhttps://www.youtube.com{i['link']}\n\n"
         await message.edit(text)
 
-    async def ytmp3xxx(message):
-        link = utils.get_arg(message)
+    async def ytmp3xxx(message, song=None):
+        link = utils.get_arg(message) if not song else song
         cmd = f"youtube2mp3 -d {os.getcwd()} -y {link}"
         process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
         for line in process.stdout:
@@ -35,3 +35,22 @@ class YouTube:
         await message.client.send_file(message.chat_id, file)
         await message.delete()
         os.remove(file)
+
+    async def songxxx(message):
+        args = utils.get_arg(message)
+        if not args:
+            await message.edit("<i>Enter a song name first</i>")
+            return
+        await message.edit("<i>Searching..</i>")
+        results = json.loads(YoutubeSearch(args, max_results=1).to_json())
+        if results:
+            await message.edit("<i>Downloading</i>")
+            link = f"https://www.youtube.com{results['videos'][0]['link']}"
+            cmd = f"youtube2mp3 -d {os.getcwd()} -y {link}"
+            process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+            process.wait()
+            await message.edit("<i>Uploading..</i>")
+            file = glob.glob("*.mp3")[0]
+            await message.client.send_file(message.chat_id, file)
+            await message.delete()
+            os.remove(file)
