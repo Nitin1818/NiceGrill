78a7f387287c1289d3c1c46391ea44960716d73e
erenmetesar <erenmetesar@gmail.com>
2019-12-23 14:04:57 +0300

    whoareyou module working with entered usernames now

--- nicegrill/modules/whoareyou.py
+++ nicegrill/modules/whoareyou.py
@@ -1,5 +1,5 @@
 import logging
-
+from nicegrill import utils
 
 class WhoAreYou:
 
@@ -7,9 +7,16 @@ class WhoAreYou:
     logger.setLevel(logging.ERROR)
 
     async def whoxxx(message):
-        user = (
-            (await message.get_reply_message()).sender if message.is_reply
-            else message.sender)
+        if not utils.get_arg(message):
+            user = (
+                (await message.get_reply_message()).sender if message.is_reply
+                else message.sender)
+        else:
+            try:
+                user = await message.client.get_entity(utils.get_arg(message))
+            except ValueError:
+                await message.edit("<i>No user found</i>")
+                return
         identify = (
             f"<b>First Name:</b> <i>{user.first_name}</i>\n"
             f"<b>Last Name:</b> <i>{user.last_name}</i>\n"
@@ -17,4 +24,4 @@ class WhoAreYou:
             f"<b>ID:</b> <i>{user.id}</i>\n"
             f"<b>Bot:</b> <i>{user.bot}</i>\n"
             f"<b>Permanent Link:</b> <a href=tg://user?id={user.id}>{user.first_name}</a>")
-        await message.reply(identify)
+        await message.edit(identify)
