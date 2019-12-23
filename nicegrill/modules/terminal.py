3e77b03c2537e09469e58f0ef8e6cee16e98242d
erenmetesar <erenmetesar@gmail.com>
2019-12-23 17:27:38 +0300

    terminal flood control added and some stuff fixed

--- nicegrill/modules/terminal.py
+++ nicegrill/modules/terminal.py
@@ -2,44 +2,53 @@ import asyncio
 import subprocess
 import logging
 from nicegrill import utils
-from telethon.errors.rpcerrorlist import MessageTooLongError
+from telethon.errors.rpcerrorlist import MessageTooLongError, MessageNotModifiedError
 
 TERMLIST = {}
 
 class Terminal:
 
+    FLOODCONTROL = 5
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.ERROR)
 
 
     async def termxxx(message):
-        output = "\n\n"
         cmd = utils.get_arg(message)
         process = subprocess.Popen(
             cmd.split(),
             stderr=subprocess.PIPE,
             stdout=subprocess.PIPE)
         template = (
-            "\n<b>⬤ Input:</b>\n\n<i>{}</i>\n\n<b>⬤ Output:</b>\n<code>"
+            "\n<b>⬤ Input:</b>\n\n<code>{}</code>\n\n<b>⬤ Output:</b>\n<code>"
             .format(cmd))
         await message.edit(template)
         if process.poll() is not None:
-            returncode = process.wait()
-            result = template + "<i>{}</i>".format(
-                "Process returned with exit code: " + str(returncode) if not process.stdout.read().decode()
-                else process.stdout.read().decode())
+            returncode = str(process.wait())
+            out = process.stdout.read().decode() + process.stderr.read().decode()
+            result = template + "{}</code>".format(
+                "Process returned with exit code: " + returncode if not out
+                else out)
             await message.edit(result)
             return
         TERMLIST.update({message.id: process})
         out = ""
         for line in process.stdout:
-            out += "<i>{}\n</i>".format(line.decode() if line.decode else process.stderr.readline().decode())
-            if len(out) < 4000 and message.id in TERMLIST:
+            if Terminal.FLOODCONTROL < 4:
+                Terminal.FLOODCONTROL += 1
+                out += "<code>{}\n</code>".format(line.decode() if line.decode else process.stderr.readline().decode())
+                continue
+            Terminal.FLOODCONTROL = 0
+            out += "<code>{}\n</code>".format(line.decode() if line.decode else process.stderr.readline().decode())
+            if len(out) < 1500 and message.id in TERMLIST:
                 await message.edit(template + out)
             else:
-                out = out[1000: -1]
+                out = out[600: -1]
                 await message.edit(template + out)
-            await asyncio.sleep(1)
+        try:
+            await message.edit(template + out)
+        except MessageNotModifiedError:
+            pass
         del TERMLIST[message.id]
 
     async def killxxx(message):
